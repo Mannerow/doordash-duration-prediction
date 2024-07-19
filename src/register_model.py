318aad81eac430635f79bc916a5cc3ed4d6c0ae3
Ubuntu <ubuntu@ip-172-31-20-33.ec2.internal>
2024-07-19 20:22:04 +0000

    Register model saves to s3 now

--- src/register_model.py
+++ src/register_model.py
@@ -2,18 +2,25 @@ import os
 import pickle
 import click
 import mlflow
+import boto3
 
 from mlflow.entities import ViewType
 from mlflow.tracking import MlflowClient
 from sklearn.linear_model import LinearRegression, Ridge
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This method reads the .env file and loads it into the environment
+
+mlflow_tracking_uri = os.getenv('MLFLOW_TRACKING_URI')
 
 HPO_EXPERIMENT_NAME = "model-hyperopt"
 TRAIN_EXPERIMENT_NAME = "model-train"
 BEST_MODELS_EXPERIMENT_NAME = "best-models"
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri(mlflow_tracking_uri)
 mlflow.set_experiment(BEST_MODELS_EXPERIMENT_NAME)
 
 
